7910c4105d43e21c2737b68e2dfbbf453d4d2f1b
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-10-15 19:59:57 +0530

    Js connected to the HTML element by using .getElementById('id')

---
+++ script.js
@@ -0,0 +1,9 @@
+const text=document.getElementById('text');
+const amount=document.getElementById('amount');
+const form=document.getElementById('form');
+const list=document.getElementById('list');
+const money_plus=document.getElementById('money-plus');
+const money_minus=document.getElementById('money-minus');
+const balance=document.getElementById('balance');
+
+
